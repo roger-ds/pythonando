bc812c62effcd5a6beb9e4d2b8907893bf162045
Rogerio Rodrigues <rogeriodemoraes@gmail.com>
2024-03-14 08:08:09 -0300

    Aulas CRUD

--- aula_django/autenticacao/models.py
+++ aula_django/autenticacao/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.db.models.fields import CharField, EmailField
 
-# Create your models here.
+
+class Pessoa(models.Model):
+    nome = CharField(max_length=100)
+    email = EmailField()
+    senha = CharField(max_length=100)
+
+
+
+# How to make Django recreate the deleted table?
+    
+# 1. Delete your migrations folder
+# 2. In the database: DELETE FROM django_migrations WHERE app = 'app_name'.
+#    You could alternatively just truncate this table.
+# 3. python manage.py makemigrations app_name
+# 4. python manage.py migrate
--- aula_django/autenticacao/views.py
+++ aula_django/autenticacao/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import json
+from hashlib import sha256
+from .models import Pessoa
 
 
 def cadastro(request):
@@ -11,4 +13,18 @@ def cadastro(request):
     elif request.method == "POST":
         nome = request.POST.get("nome")
         email = request.POST.get("email")
-        return HttpResponse(json.dumps({"nome": nome, "email": email}))
+        senha = sha256(request.POST.get("senha").encode()).hexdigest()
+        pessoa = Pessoa(nome=nome, email=email, senha=senha)
+        pessoa.save()
+        return HttpResponse(json.dumps({"nome": nome, 
+                                        "email": email, 
+                                        "senha": senha}))
+
+
+def listar(request):
+    # pessoas = (Pessoa.objects.filter(senha="96020064") | Pessoa.objects.filter(nome="Lu")).exclude(senha="12345678")
+    excluir = Pessoa.objects.filter(nome="Lu").filter(senha="12345678")[0]
+    excluir.delete()
+    
+    pessoas = Pessoa.objects.all()
+    return render(request, "listar/pessoas.html", {"pessoas": pessoas})
